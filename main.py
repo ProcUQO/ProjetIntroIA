b090ac846d1355462610286f53129228bfcee851
WilliamLamoureux <williamlamoureuxv2@gmail.com>
2026-04-12 15:05:03 -0400

    Finalisation de la mécanique de dessin

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import tkinter as gui
 
 #voilà les info et dimension de l'écran de l'application
 app = gui.Tk()
-app.geometry("800x500")
+app.geometry("1000x600")
 app.configure(background='lightgray')
 app.title("Brain age project")
 
@@ -19,30 +19,54 @@ frameGeneral.columnconfigure(0, weight=2)
 frameGeneral.columnconfigure(1, weight=1)
 frameGeneral.rowconfigure(0, minsize= 450)
 
+
 #Conteneur pour les question qui vont être poser
 frameQuestion = gui.Frame(frameGeneral)
 frameQuestion.configure(background='pink')
 frameQuestion.columnconfigure(0, minsize=300)
-frameQuestion.rowconfigure(0, minsize= 440)
+frameQuestion.rowconfigure(0, minsize= 465)
 
 #Conteneur où on va pouvoir dessiner pour répondre aux questions.
-frameDessin = gui.Frame(frameGeneral)
-frameDessin.configure(background='lightblue')
-frameDessin.columnconfigure(0, minsize=500)
-frameDessin.rowconfigure(0, minsize= 440)
+conteneurDessin = gui.Frame(frameGeneral)
+conteneurDessin.configure(background='lightgrey')
+conteneurDessin.columnconfigure(0, minsize=300)
+conteneurDessin.rowconfigure(0, minsize= 300)
+conteneurDessin.rowconfigure(1, minsize= 25)
 
+#créé un canevas pour qu'on puisse répondre au question en dessinant
+frameDessin = gui.Canvas(conteneurDessin, width=300, height= 435)
+frameDessin.configure(background='lightBlue')
+
+#Permet d'effacer notre réponse si on c'est tromper
+#Ou on peut l'utiliser quand on change de question
+def ClearDessin():
+    print('Je delete')
+    frameDessin.delete('all')
+
+#crée un bouton pour effacer notre réponse si on s'est tromper
+clearButton = gui.Button(conteneurDessin, text='Clear', width=42, command=ClearDessin)
+clearButton.configure(background='lightgreen')
+
+#Affiche un texte dans la grid de nos question
 textQuestion = gui.Label(frameQuestion, text="Zone de questions", font=('Arial', 16))
 textQuestion.configure(background='pink')
 textQuestion.grid(row=0, column=0, padx=5, pady=5)
 
-textDessin = gui.Label(frameDessin, text="Zone de dessin", font=('Arial', 16))
-textDessin.configure(background='lightblue')
-textDessin.grid(row=0, column=0, padx=5, pady=5)
-
+#permet d'organiser l'interface avec les colonne et ranger des grilles
 frameGeneral.pack()
-frameDessin.grid(row=0, column=1)
 frameQuestion.grid(row=0, column=0)
+conteneurDessin.grid(row=0, column=1)
+frameDessin.grid(row=0, column=0)
+clearButton.grid(row=1, column=0)
 
-drawSpace = gui.Canvas
+
+#permet d'instancier des rectangles de grosseur défini quand on maintien le click.
+def Drawing(event):
+    posX = event.x
+    posY = event.y
+    
+    frameDessin.create_rectangle((posX,posY),(posX+2,posY+2), fill='black')
+
+frameDessin.bind("<B1-Motion>", Drawing)
 
 app.mainloop()
